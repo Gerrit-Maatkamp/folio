0d4030a09009f3f64a1e21e0af6e4477e07f4483
Gerrit-Maatkamp <gerrit.r.maatkamp@gmail.com>
2024-06-17 10:59:07 -0700

    Commit to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const baseUrl = import.meta.env.BASE_URL;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+console.log(`Base URL: ${baseUrl}`);
